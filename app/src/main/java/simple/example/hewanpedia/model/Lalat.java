41c577429d08f67d102d80a7db1920b72e688f29
taufiqurrahman <muh.taufiqurrahman2019@student.hamzanwadi.ac.id>
2020-11-25 10:07:51 +0700

    taufik

---
+++ app/src/main/java/simple/example/hewanpedia/model/Lalat.java
@@ -0,0 +1,10 @@
+package simple.example.hewanpedia.model;
+
+public class Lalat extends Hewan{
+
+    public Lalat(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Lalat",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
